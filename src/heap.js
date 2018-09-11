463e1f421a5d647b60158f5f843123123c43c2d3
xiaoping-li <narci2008@gmail.com>
2018-09-10 17:00:15 -0700

    Add getMax() and getSize() for heap.js

--- src/heap.js
+++ src/heap.js
@@ -9,30 +9,36 @@ class Heap {
     insert(val) {
 
     }
+
     // Deletes the element located at the front of the heap (the max if a max heap, or a min if a min heap)
     // Calls siftDown in order to reorganize the heap with a new max/min
     // In some specifications, this method is also called `poll`
     delete() {
 
     }
+
     // Returns the maximum value in the heap in constant time
     getMax() {
-
+        return this.storage[0];
     }
+
     // Returns the size of the heap
     getSize() {
-
+        return this.size;
     }
+
     // Returns the storage array
     getStorage() {
 
     }
+
     // Moves the element at the specified index "up" by swapping it with its parent 
     // if its parent value is less than the value located at the input index
     // This method is only used by the heap itself in order to maintain the heap property
     bubbleUp(index) {
 
     }
+
     // First grabs the indices of this element's children and determines which of the children are larger
     // If the larger of the child elements is larger than the parent, the child element is swapped with the parent
     // This method is only used by the heap itself in order to maintain the heap property
