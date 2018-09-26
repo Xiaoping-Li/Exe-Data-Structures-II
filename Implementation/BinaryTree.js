960b5571fd00196d0cfc56967eb5317d9731c342
Xiaoping-Li <narci2008@gmail.com>
2018-09-26 13:45:12 -0700

    Update BinaryTree

--- Implementation/BinaryTree.js
+++ Implementation/BinaryTree.js
@@ -4,4 +4,26 @@ class BinaryTree {
     this.left = null;
     this.right = null;
   }
+  
+  insert(value) {
+    const newNode = new BinaryTree(value);
+    if (!this.value) {
+      this.value = value;
+    } else {
+      if (value <= this.value) {
+      if (!this.left) {
+          this.left = newNode;
+        } else {
+          this.left.insert(value);
+        }
+      } else {
+        if (!this.right) {
+          this.right = newNode;
+        } else {
+          this.right.insert(value);
+        }
+      }
+    }  
+    return newNode;
+  }
 }
